0f2a42f1e28d23107f3297c4cc41aef584eb1e96
Samira <samira.shabanoska@gmail.com>
2026-04-10 10:17:14 +0200

    Frontent start

---
+++ script.js
@@ -0,0 +1,78 @@
+// Nav
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+// Ham
+
+    const hamburger = document.querySelector(".hamburger");
+    const navContent = document.querySelector(".nav-content");
+
+    if (hamburger && navContent) {
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navContent.classList.toggle("active");
+
+            const isOpened = hamburger.getAttribute("aria-expanded") === "true";
+            hamburger.setAttribute("aria-expanded", !isOpened);
+        });
+
+        const navLinks = document.querySelectorAll(".nav-links a");
+        navLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navContent.classList.remove("active");
+                hamburger.setAttribute("aria-expanded", "false");
+            });
+        });
+    }
+});
+
+// Gallery
+
+const gallery = document.querySelector('.hero-gallery');
+
+if (gallery) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.70
+    });
+
+    observer.observe(gallery);
+}
+
+// Booklet
+
+const booklet = document.querySelector('.booklet img');
+
+if (booklet) {
+    const bookletObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-visible');
+            } else {
+                entry.target.classList.remove('fade-in-visible');
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    bookletObserver.observe(booklet);
+}
