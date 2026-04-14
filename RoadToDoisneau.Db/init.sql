4ba9cb37be6f148ed7b03e5568fca412cd242e96
Fabio <catanzarofabio.2006@gmail.com>
2026-04-14 12:33:33 +0200

    Aggiunto Db

--- RoadToDoisneau.Db/init.sql
+++ RoadToDoisneau.Db/init.sql
@@ -1,1 +1,46 @@
-CREATE TABLE TEST ();
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(255) NOT NULL,
+    title VARCHAR(75) NOT NULL,
+    location VARCHAR(75),
+    description TEXT,
+    shooting_year INTEGER NOT NULL
+);
+
+CREATE TABLE prices (
+	id SERIAL PRIMARY KEY,
+	category VARCHAR(50) NOT NULL,
+	price DECIMAL(10, 2) NOT NULL);
+
+CREATE TABLE tickets (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    holder_first_name VARCHAR(75) NOT NULL,
+    holder_last_name VARCHAR(75) NOT NULL,
+    holder_mail VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP + INTERVAL '6 months'),
+    has_booklet BOOL NOT NULL DEFAULT False,
+    fk_price_id INT NOT NULL REFERENCES prices(id)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    note TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE, -- Suggerito ON DELETE CASCADE
+    ticket_id UUID REFERENCES tickets(id) ON DELETE SET NULL
+);
+
+CREATE TABLE articles (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(150) NOT NULL,
+	description TEXT NOT NULL DEFAULT DIOPORCO,
+	page VARCHAR(255) NOT NULL,
+	data TIMESTAMP DEFAULT CURRENT TIMESTAMP,
+	author VARCHAR(100),
+	thumbnail VARCHAR(255) NOT NULL
+);
