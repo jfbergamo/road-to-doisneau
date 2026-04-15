9b496f38ddaae5da043a3d3bec4b5f123e601914
J <jacopobergamasco06@gmail.com>
2026-04-15 17:12:42 +0200

    aggiunta classe di endpoint

---
+++ RoadToDoisneau.Backend/Endpoints/ArticlesEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace RoadToDoisneau.Backend.Endpoints;
+
+public static class ArticlesEndpoints
+{
+    public static void MapArticlesEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/articles").WithTags("Articles");
+
+        group.MapGet("", GetArticlesAsync)
+             .WithName("GetArticles");
+    }
+
+    private static async Task<Typed>
+}
