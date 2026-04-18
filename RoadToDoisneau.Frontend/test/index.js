4837b387cb8a174e8b84824c5a9fde58cdbfb1c3
J <jacopobergamasco06@gmail.com>
2026-04-18 16:05:53 +0200

    test: invio di dati non-json per vedere la risposta

--- RoadToDoisneau.Frontend/test/index.js
+++ RoadToDoisneau.Frontend/test/index.js
@@ -3,19 +3,20 @@ const button = document.getElementById("btn");
 const API_ENDPOINT = "https://localhost:7022/api"
 
 const order = {
-  "diego betto": 1,
-  "tickets": [
-    {
-      "holderName": "string",
-      "bullshit": "string",
-      "hasBooklet": true,
-      "discountCode": "FREE",
-    }
-  ]
+    "diego betto": 1,
+    "tickets": [
+        {
+            "holderName": "string",
+            "bullshit": "string",
+            "hasBooklet": true,
+            "discountCode": "FREE",
+        }
+    ]
 }
 
 button.addEventListener("click", async () => {
-    const body = JSON.stringify(order);
+    // const body = JSON.stringify(order);
+    const body = "bullshit";
     console.log(`Sending ${body}`);
     const result = await fetch(`${API_ENDPOINT}/orders`, {
         method: "POST",
