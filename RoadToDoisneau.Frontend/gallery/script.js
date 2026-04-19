0a61420a8d3e7261439992c5503f391ca34aabaf
Samira <samira.shabanoska@gmail.com>
2026-04-19 20:18:02 +0200

    refactor gallery nav

--- RoadToDoisneau.Frontend/gallery/script.js
+++ RoadToDoisneau.Frontend/gallery/script.js
@@ -1,3 +1,41 @@
+// Nav
+document.addEventListener('DOMContentLoaded', () => {
+
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+    // Ham
+    const hamburger = document.querySelector(".nav-hamburger");
+    const navContent = document.querySelector(".nav-content");
+
+    if (hamburger && navContent) {
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navContent.classList.toggle("active");
+
+            const isOpened = hamburger.getAttribute("aria-expanded") === "true";
+            hamburger.setAttribute("aria-expanded", !isOpened);
+        });
+
+        const navLinks = document.querySelectorAll(".nav-links a");
+        navLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navContent.classList.remove("active");
+                hamburger.setAttribute("aria-expanded", "false");
+            });
+        });
+    }
+});
+
+
 localStorage.clear();
 
 localStorage.clear();
@@ -32,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             const imgPath = item.querySelector('img').src;
             const text = item.querySelector('.gallery-caption').innerText;
-            
+
             modalImg.src = imgPath;
             modalCaption.innerText = text;
             modal.style.display = "block";
