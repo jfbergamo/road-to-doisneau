e07d91803317052a31122145e2b7d5056a28b5eb
J <jacopobergamasco06@gmail.com>
2026-04-22 09:57:00 +0200

    refactor mostra/script.js

--- RoadToDoisneau.Frontend/mostra/script.js
+++ RoadToDoisneau.Frontend/mostra/script.js
@@ -70,7 +70,12 @@ function updateCountdown() {
 
     if (distance < 0) {
         clearInterval(countdownInterval);
-        document.querySelector(".countdown-container").innerHTML = "<h3>Mostra Conclusa!</h3>";
+        const countdownContainer = document.querySelector(".countdown-container");
+        {
+            const h3 = document.createElement('h3');
+            h3.innerText = 'Mostra Conclusa!';
+            countdownContainer.appendChild(h3);
+        }
         return;
     }
 
