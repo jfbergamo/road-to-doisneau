2b3f649c7bec064e612f9bf8492235af1869a5e7
J <jacopobergamasco06@gmail.com>
2026-04-22 14:22:49 +0200

    fixata posizione di default menu

--- RoadToDoisneau.Frontend/accesibility.js
+++ RoadToDoisneau.Frontend/accesibility.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-        new Accessibility({
-            position: {
-                bottom: { size: 20, units: 'px' },
-                left: { size: 20, units: 'px' },
-                right: 'auto'
-            },
-            session: {
-                persistent: true
-            },
-            baseFont: 16 
-        });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#accessibility-modal')?.classList.add('bottom');
+});
